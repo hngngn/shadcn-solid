50420b56341ded437cb1e53d9f492d5def6715ff
hngngn <huyhoang.hngngn@gmail.com>
2023-06-22 21:32:26 +0700

    redirect to introduction, for now

--- apps/docs/src/routes/index.tsx
+++ apps/docs/src/routes/index.tsx
@@ -1,7 +1,5 @@
+import { Navigate } from "solid-start"
+
 export default () => {
-	return (
-		<div class="items-center justify-center flex min-h-inherit">
-			<h1>Work in progress</h1>
-		</div>
-	)
+	return <Navigate href="/docs/introduction" />
 }
