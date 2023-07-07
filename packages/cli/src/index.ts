5a3de08f4b9bd132d2076271f800b72f450ec1b4
hngngn <huyhoang.hngngn@gmail.com>
2023-07-07 13:10:13 +0700

    fix(cli): `package.json` not found

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -3,22 +3,14 @@ import { add } from "@/src/commands/add"
 import { init } from "@/src/commands/init"
 import { Command } from "commander"
 
-import { getPackageInfo } from "./utils/get-package-info"
-
 process.on("SIGINT", () => process.exit(0))
 process.on("SIGTERM", () => process.exit(0))
 
 async function main() {
-    const packageInfo = getPackageInfo()
-
     const program = new Command()
         .name("shadcn-solid")
         .description("add components and dependencies to your project")
-        .version(
-            packageInfo.version!,
-            "-v, --version",
-            "display the version number"
-        )
+        .version("0.1.0", "-v, --version", "display the version number")
 
     program.addCommand(init).addCommand(add)
 
--- packages/cli/src/utils/get-package-info.ts
+++
@@ -1,9 +0,0 @@
-import fs from "fs-extra"
-import path from "path"
-import { type PackageJson } from "type-fest"
-
-export function getPackageInfo() {
-    const packageJsonPath = path.join("package.json")
-
-    return fs.readJSONSync(packageJsonPath) as PackageJson
-}
