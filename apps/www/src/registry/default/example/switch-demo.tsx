45b5e6b34feb348018de251f64b7c87cffdb42bf
hngngn <huyhoang.hngngn@gmail.com>
2024-03-04 13:16:57 +0700

    docs: fix `switch` focus

--- apps/www/src/registry/default/example/switch-demo.tsx
+++ apps/www/src/registry/default/example/switch-demo.tsx
@@ -6,7 +6,7 @@ const SwitchDemo = () => {
       <SwitchControl>
         <SwitchThumb />
       </SwitchControl>
-      <SwitchLabel class="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+      <SwitchLabel class="text-sm font-medium leading-none data-[disabled]:cursor-not-allowed data-[disabled]:opacity-70">
         Airplane Mode
       </SwitchLabel>
     </Switch>
