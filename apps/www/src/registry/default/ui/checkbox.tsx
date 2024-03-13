d51cdc8fcab5e5f5203b7b0919dc969d860a0dd4
hngngn <huyhoang.hngngn@gmail.com>
2024-03-13 15:39:33 +0700

    fix: `Checkbox` focus

--- apps/www/src/registry/default/ui/checkbox.tsx
+++ apps/www/src/registry/default/ui/checkbox.tsx
@@ -12,25 +12,33 @@ export const CheckboxControl: ParentComponent<CheckboxPrimitive.CheckboxControlP
   const [local, rest] = splitProps(props, ["class", "children"]);
 
   return (
-    <CheckboxPrimitive.Control
-      class={cn(
-        "h-4 w-4 shrink-0 rounded-sm border border-primary shadow focus-visible:outline-none focus-visible:ring focus-visible:ring-ring data-[disabled]:cursor-not-allowed data-[checked]:bg-primary data-[checked]:text-primary-foreground data-[disabled]:opacity-50",
-        local.class
-      )}
-      {...rest}
-    >
-      <CheckboxPrimitive.Indicator class="flex items-center justify-center text-current">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="h-4 w-4">
-          <path
-            fill="none"
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="m5 12l5 5L20 7"
-          />
-        </svg>
-      </CheckboxPrimitive.Indicator>
-    </CheckboxPrimitive.Control>
+    <>
+      <CheckboxPrimitive.Input
+        class={cn(
+          "[&:focus-visible+div]:outline-none [&:focus-visible+div]:ring-2 [&:focus-visible+div]:ring-ring [&:focus-visible+div]:ring-offset-2 [&:focus-visible+div]:ring-offset-background",
+          local.class
+        )}
+      />
+      <CheckboxPrimitive.Control
+        class={cn(
+          "h-4 w-4 shrink-0 rounded-sm border border-primary shadow focus-visible:outline-none focus-visible:ring focus-visible:ring-ring data-[disabled]:cursor-not-allowed data-[checked]:bg-primary data-[checked]:text-primary-foreground data-[disabled]:opacity-50",
+          local.class
+        )}
+        {...rest}
+      >
+        <CheckboxPrimitive.Indicator class="flex items-center justify-center text-current">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="h-4 w-4">
+            <path
+              fill="none"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="m5 12l5 5L20 7"
+            />
+          </svg>
+        </CheckboxPrimitive.Indicator>
+      </CheckboxPrimitive.Control>
+    </>
   );
 };
