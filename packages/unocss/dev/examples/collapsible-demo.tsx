be058fd043d0978018241817de13d752ad5ea825
hngngn <huyhoang.hngngn@gmail.com>
2024-05-06 13:26:31 +0700

    refactor: `Collapsible`

--- packages/unocss/dev/examples/collapsible-demo.tsx
+++ packages/unocss/dev/examples/collapsible-demo.tsx
@@ -1,4 +1,3 @@
-import { As } from "@kobalte/core";
 import { createSignal } from "solid-js";
 import { Button } from "../../ui/button";
 import { Collapsible, CollapsibleContent, CollapsibleTrigger } from "../../ui/collapsible";
@@ -10,8 +9,7 @@ const CollapsibleDemo = () => {
     <Collapsible open={isOpen()} onOpenChange={setIsOpen} class="w-350px space-y-2">
       <div class="flex items-center justify-between space-x-4 px-4">
         <h4 class="text-sm font-semibold">@RyanCarniato starred 2 repositories</h4>
-        <CollapsibleTrigger asChild>
-          <As component={Button} variant="ghost" size="sm" class="w-9 p-0">
+        <CollapsibleTrigger as={() => <Button variant="ghost" size="sm" class="w-9 p-0">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" viewBox="0 0 24 24">
               <path
                 fill="none"
@@ -23,8 +21,7 @@ const CollapsibleDemo = () => {
               />
             </svg>
             <span class="sr-only">Toggle</span>
-          </As>
-        </CollapsibleTrigger>
+          </Button>}/>
       </div>
       <div class="rounded-md border px-4 py-3 font-mono text-sm">solid-js</div>
       <CollapsibleContent class="space-y-2">
