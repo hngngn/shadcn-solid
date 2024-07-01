cac99eff48de6a0de9239dee8e935aca2cf381d5
hngngn <huyhoang.hngngn@gmail.com>
2024-07-01 13:04:18 +0700

    feat: setup vercel and solid

--- apps/www/astro.config.mjs
+++ apps/www/astro.config.mjs
@@ -1,4 +1,10 @@
-import { defineConfig } from 'astro/config';
+import solidJs from "@astrojs/solid-js";
+import vercel from "@astrojs/vercel/serverless";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "server",
+  adapter: vercel(),
+  integrations: [solidJs()]
+});
