8556670878c650383d202ede9f10be1ce2ebee7c
hngngn <huyhoang.hngngn@gmail.com>
2024-08-22 12:11:55 +0700

    feat: add `indeterminate` state

--- packages/tailwindcss/solid/ui/checkbox.tsx
+++ packages/tailwindcss/solid/ui/checkbox.tsx
@@ -1,68 +1,95 @@
 import { cn } from "@/libs/cn";
 import type {
 	CheckboxControlProps,
+	CheckboxDescriptionProps,
+	CheckboxErrorMessageProps,
+	CheckboxLabelProps,
 	CheckboxRootProps,
 } from "@kobalte/core/checkbox";
 import { Checkbox as CheckboxPrimitive } from "@kobalte/core/checkbox";
 import type { PolymorphicProps } from "@kobalte/core/polymorphic";
-import type {
-	JSXElement,
-	ParentProps,
-	ValidComponent,
-	VoidProps,
-} from "solid-js";
-import { Show, splitProps } from "solid-js";
+import type { JSXElement, ValidComponent, VoidProps } from "solid-js";
+import { Match, Switch, splitProps } from "solid-js";
 
-export type checkboxProps<T extends ValidComponent = "div"> = ParentProps<
+export type checkboxProps<T extends ValidComponent = "div"> =
 	CheckboxRootProps<T> & {
 		class?: string;
-		title?: string;
-		description?: string;
-		error?: string;
-	}
->;
+	};
 
 export const Checkbox = <T extends ValidComponent = "div">(
 	props: PolymorphicProps<T, checkboxProps<T>>,
 ) => {
-	const [local, rest] = splitProps(props as checkboxProps, [
+	const [local, rest] = splitProps(props as checkboxProps, ["class"]);
+
+	return (
+		<CheckboxPrimitive class={cn("flex gap-x-2", local.class)} {...rest} />
+	);
+};
+
+export type checkboxLabelProps<T extends ValidComponent = "label"> =
+	CheckboxLabelProps<T> & {
+		class?: string;
+	};
+
+export const CheckboxLabel = <T extends ValidComponent = "label">(
+	props: PolymorphicProps<T, checkboxLabelProps<T>>,
+) => {
+	const [local, rest] = splitProps(props as checkboxLabelProps, ["class"]);
+
+	return (
+		<CheckboxPrimitive.Label
+			class={cn("text-sm font-medium select-none", local.class)}
+			{...rest}
+		/>
+	);
+};
+
+export type checkboxDescriptionProps<T extends ValidComponent = "div"> =
+	CheckboxDescriptionProps<T> & {
+		class?: string;
+	};
+
+export const CheckboxDescription = <T extends ValidComponent = "div">(
+	props: PolymorphicProps<T, checkboxDescriptionProps<T>>,
+) => {
+	const [local, rest] = splitProps(props as checkboxDescriptionProps, [
 		"class",
-		"children",
-		"title",
-		"description",
-		"error",
 	]);
 
 	return (
-		<CheckboxPrimitive class={cn("flex gap-x-2", local.class)} {...rest}>
-			{local.children}
-			<Show
-				when={
-					local.title || local.description || rest.validationState === "invalid"
-				}
-			>
-				<div class="flex flex-col gap-y-1">
-					<Show when={local.title}>
-						<CheckboxPrimitive.Label class="text-sm font-medium select-none grid-col-end-0">
-							{local.title}
-						</CheckboxPrimitive.Label>
-					</Show>
-					<Show when={local.description}>
-						<CheckboxPrimitive.Description class="text-sm/6 text-muted-foreground select-none grid-col-start-2">
-							{local.description}
-						</CheckboxPrimitive.Description>
-					</Show>
-					<CheckboxPrimitive.ErrorMessage class="text-xs/6 text-red-500 font-medium grid-col-start-2">
-						{local.error}
-					</CheckboxPrimitive.ErrorMessage>
-				</div>
-			</Show>
-		</CheckboxPrimitive>
+		<CheckboxPrimitive.Description
+			class={cn("text-sm/6 text-muted-foreground select-none", local.class)}
+			{...rest}
+		/>
+	);
+};
+
+export type checkboxErrorMessageProps<T extends ValidComponent = "div"> =
+	CheckboxErrorMessageProps<T> & {
+		class?: string;
+	};
+
+export const CheckboxErrorMessage = <T extends ValidComponent = "div">(
+	props: PolymorphicProps<T, CheckboxErrorMessageProps<T>>,
+) => {
+	const [local, rest] = splitProps(props as checkboxErrorMessageProps, [
+		"class",
+	]);
+
+	return (
+		<CheckboxPrimitive.ErrorMessage
+			class={cn("text-xs/6 text-red-500 font-medium", local.class)}
+			{...rest}
+		/>
 	);
 };
 
 export type checkboxControlProps<T extends ValidComponent = "div"> = VoidProps<
-	CheckboxControlProps<T> & { class?: string; icon?: JSXElement }
+	CheckboxControlProps<T> & {
+		class?: string;
+		icon?: JSXElement;
+		indeterminate?: boolean;
+	}
 >;
 
 export const CheckboxControl = <T extends ValidComponent = "div">(
@@ -72,6 +99,7 @@ export const CheckboxControl = <T extends ValidComponent = "div">(
 		"class",
 		"children",
 		"icon",
+		"indeterminate",
 	]);
 
 	return (
@@ -82,7 +110,7 @@ export const CheckboxControl = <T extends ValidComponent = "div">(
 					"[--checkbox-control-border:theme(colors.zinc.300)] [--checkbox-control-bg:theme(colors.zinc.50)] [--checkbox-control-text:theme(colors.white)]",
 					"peer-focus-visible:outline-none peer-focus-visible:ring-4 peer-focus-visible:ring-blue-200 peer-focus-visible:[--checkbox-control-border:theme(colors.blue.500)]",
 					"peer-hover:[--checkbox-control-bg:theme(colors.zinc.200)] hover:[--checkbox-control-bg:theme(colors.zinc.200)]",
-					"data-[checked]:[--checkbox-control-bg:theme(colors.blue.500)] data-[checked]:peer-hover:[--checkbox-control-bg:theme(colors.blue.500)] data-[checked]:hover:[--checkbox-control-bg:theme(colors.blue.500)] data-[checked]:border-[--checkbox-control-bg]",
+					"has-[[data-checked],[data-indeterminate]]:[--checkbox-control-bg:theme(colors.blue.500)] has-[[data-checked],[data-indeterminate]]:peer-hover:[--checkbox-control-bg:theme(colors.blue.500)] has-[[data-checked],[data-indeterminate]]:hover:[--checkbox-control-bg:theme(colors.blue.500)] has-[[data-checked],[data-indeterminate]]:border-[--checkbox-control-bg]",
 					"data-[invalid]:[--checkbox-control-bg:theme(colors.red.100)] data-[invalid]:[--checkbox-control-border:theme(colors.red.400)] data-[invalid]:peer-hover:[--checkbox-control-bg:theme(colors.red.300)] data-[invalid]:hover:[--checkbox-control-bg:theme(colors.red.300)] data-[invalid]:peer-focus-visible:ring-red-200 data-[invalid]:peer-focus-visible:[--checkbox-control-border:theme(colors.red.500)]",
 					"has-[[data-checked][data-invalid]]:[--checkbox-control-bg:theme(colors.red.500)] has-[[data-checked][data-invalid]]:peer-hover:[--checkbox-control-bg:theme(colors.red.500)] has-[[data-checked][data-invalid]]:hover:[--checkbox-control-bg:theme(colors.red.500)]",
 					"size-4 shrink-0 rounded-sm transition-[box-shadow,border-color,background-color,color] duration-300 border border-[--checkbox-control-border] bg-[--checkbox-control-bg] text-[--checkbox-control-text]",
@@ -94,9 +122,8 @@ export const CheckboxControl = <T extends ValidComponent = "div">(
 					class="flex items-center justify-center size-full [&>svg]:animate-in [&>svg]:zoom-in [&>svg]:fade-in-0 [&>svg]:duration-300"
 					data-checked
 				>
-					<Show
-						when={local.icon}
-						fallback={
+					<Switch>
+						<Match when={!local.icon && !local.indeterminate}>
 							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
 								<path
 									fill="none"
@@ -108,10 +135,24 @@ export const CheckboxControl = <T extends ValidComponent = "div">(
 								/>
 								<title>Checked</title>
 							</svg>
-						}
-					>
-						{local.icon}
-					</Show>
+						</Match>
+						<Match when={local.icon && !local.indeterminate}>
+							{local.icon}
+						</Match>
+						<Match when={local.indeterminate}>
+							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+								<path
+									fill="none"
+									stroke="currentColor"
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									stroke-width="2"
+									d="M5 12h14"
+								/>
+								<title>Indeterminate</title>
+							</svg>
+						</Match>
+					</Switch>
 				</CheckboxPrimitive.Indicator>
 			</CheckboxPrimitive.Control>
 		</div>
--- packages/unocss/dev/checkbox.stories.tsx
+++ packages/unocss/dev/checkbox.stories.tsx
@@ -2,11 +2,21 @@ import type { ComponentProps } from "solid-js";
 import {
 	Checkbox,
 	CheckboxControl,
+	CheckboxDescription,
+	CheckboxErrorMessage,
+	CheckboxLabel,
 	type checkboxProps,
 } from "../solid/ui/checkbox";
 import type { Meta, StoryObj } from "../types";
 
-type Story = StoryObj<checkboxProps & { icon: 1 | 2 }>;
+type Story = StoryObj<
+	checkboxProps & {
+		icon: 1 | 2;
+		title: string;
+		description: string;
+		error: string;
+	}
+>;
 
 export const Default: Story = {
 	args: {
@@ -15,6 +25,7 @@ export const Default: Story = {
 		error: "You must agree to our Terms and Conditions.",
 		validationState: "valid",
 		icon: 1,
+		indeterminate: false,
 	},
 };
 
@@ -22,31 +33,41 @@ export default {
 	title: "UnoCSS/Solid/Checkbox",
 	render: (props) => (
 		<Checkbox {...props}>
-			<CheckboxControl
-				icon={
-					props.icon === 1 ? (
-						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-							<path
-								fill="none"
-								stroke="currentColor"
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								stroke-width="2"
-								d="m5 12l5 5L20 7"
-							/>
-							<title>Checked</title>
-						</svg>
-					) : (
-						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-							<path
-								fill="currentColor"
-								d="M6.979 3.074a6 6 0 0 1 4.988 1.425l.037.033l.034-.03a6 6 0 0 1 4.733-1.44l.246.036a6 6 0 0 1 3.364 10.008l-.18.185l-.048.041l-7.45 7.379a1 1 0 0 1-1.313.082l-.094-.082l-7.493-7.422A6 6 0 0 1 6.979 3.074"
-							/>
-							<title>Checked</title>
-						</svg>
-					)
-				}
-			/>
+			{({ indeterminate }) => (
+				<>
+					<CheckboxControl
+						indeterminate={indeterminate()}
+						icon={
+							props.icon === 1 ? (
+								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+									<path
+										fill="none"
+										stroke="currentColor"
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										stroke-width="2"
+										d="m5 12l5 5L20 7"
+									/>
+									<title>Checked</title>
+								</svg>
+							) : props.icon === 2 ? (
+								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+									<path
+										fill="currentColor"
+										d="M6.979 3.074a6 6 0 0 1 4.988 1.425l.037.033l.034-.03a6 6 0 0 1 4.733-1.44l.246.036a6 6 0 0 1 3.364 10.008l-.18.185l-.048.041l-7.45 7.379a1 1 0 0 1-1.313.082l-.094-.082l-7.493-7.422A6 6 0 0 1 6.979 3.074"
+									/>
+									<title>Checked</title>
+								</svg>
+							) : undefined
+						}
+					/>
+					<div class="flex flex-col gap-y-1">
+						<CheckboxLabel>{props.title}</CheckboxLabel>
+						<CheckboxDescription>{props.description}</CheckboxDescription>
+						<CheckboxErrorMessage>{props.error}</CheckboxErrorMessage>
+					</div>
+				</>
+			)}
 		</Checkbox>
 	),
 	argTypes: {
@@ -60,7 +81,7 @@ export default {
 			control: {
 				type: "radio",
 			},
-			options: [1, 2],
+			options: [1, 2, 3],
 		},
 	},
 } satisfies Meta<ComponentProps<typeof Checkbox>>;
