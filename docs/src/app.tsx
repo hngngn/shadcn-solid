fa73f40d9ecab334a811e78fc80c9881447f3dc9
hngngn <huyhoang.hngngn@gmail.com>
2024-12-10 13:50:53 +0700

    fix: iframe dark mode

--- docs/src/app.tsx
+++ docs/src/app.tsx
@@ -1,4 +1,8 @@
-import { ColorModeProvider, ColorModeScript } from "@kobalte/core";
+import {
+	ColorModeProvider,
+	ColorModeScript,
+	cookieStorageManagerSSR,
+} from "@kobalte/core";
 import { Toaster } from "@repo/tailwindcss/ui/sonner";
 import { ToastList, ToastRegion } from "@repo/tailwindcss/ui/toast";
 import { MetaProvider } from "@solidjs/meta";
@@ -9,15 +13,27 @@ import { Metadata } from "~/components/metadata";
 
 import "./app.css";
 import "./mdx.css";
+import { isServer } from "solid-js/web";
+import { getCookie } from "vinxi/http";
+
+const getServerCookies = () => {
+	"use server";
+	const colorMode = getCookie("kb-color-mode");
+	return colorMode ? `kb-color-mode=${colorMode}` : "";
+};
 
 export default function App() {
+	const storageManager = cookieStorageManagerSSR(
+		isServer ? getServerCookies() : document.cookie,
+	);
+
 	return (
 		<Router
 			root={(props) => (
 				<MetaProvider>
 					<Metadata />
-					<ColorModeScript />
-					<ColorModeProvider>
+					<ColorModeScript storageType={storageManager.type} />
+					<ColorModeProvider storageManager={storageManager}>
 						<Suspense>{props.children}</Suspense>
 						<ToastRegion>
 							<ToastList />
